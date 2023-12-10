0866d06eff2e43fc8384bafd62fed9fe888ca9ab
[AntonellaCrestan] <[antocrestan@hotmail.com]>
2023-12-10 17:26:39 -0300

    actualización de frontend previa a conexion con backend

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,7 +3,12 @@
 var PORT    = 3000;
 
 var express = require('express');
+var cors = require ("cors");
+var corsOptions = {origin:"*",optionSuccessStatus:200};
+
 var app     = express();
+app.use(cors(corsOptions));
+
 var utils   = require('./mysql-connector');
 
 // to parse application/json
@@ -12,6 +17,33 @@ app.use(express.json());
 app.use(express.static('/home/node/app/static/'));
 
 //=======[ Main module code ]==================================================
+
+app.get("/otraCosa/:id/:name",(req,res,next) => {
+    console.log("id",req.params.id)
+    console.log("name",req.params.name)
+    utils.query("select * from Devices where id="+req.params.id,(err,rsp,fields)=>{
+        if(err==null){
+            console.log("rsp",rsp);
+            res.status(200).send(JSON.stringify(rsp));
+        }else{
+            console.log("err",err);
+            res.status(409).send(err);
+        }
+
+        //console.log(fields);
+    });
+
+});    
+
+app.post("/device",(req,res,next) => {
+    console.log("Llego el post",
+    "UPDATE Devices SET state = "+req.body.state+" WHERE id="+req.body.id);
+    if(req.body.name==""){
+        res.status(409).send("no tengo nada que hacer");
+    }else{
+        res.status(200).send("se guardo el dispositivo");
+    }  
+});
 
 app.get('/devices/', function(req, res, next) {
     devices = [
@@ -27,7 +59,14 @@ app.get('/devices/', function(req, res, next) {
             'name': 'Ventilador 1', 
             'description': 'Ventilador Habitacion', 
             'state': 1, 
-            'type': 2, 
+            'type': 2,
+        },
+        {
+            'id': 3, 
+            'name': 'TV', 
+            'description': 'Televisor cocina', 
+            'state': 0, 
+            'type': 3, 
         },
     ]
     res.send(JSON.stringify(devices)).status(200);
--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,7 +1,8 @@
+var M;
 class Main implements EventListenerObject {
     public usuarios: Array<Usuario> = new Array<Usuario>();
 
-    private buscarPersonas() {
+    private buscarPersonas() { //Función para cargar los nombres de usuario en área de texto de Smart Home 
        
         var textarea=document.getElementById("textarea_1") as HTMLTextAreaElement;
 
@@ -10,7 +11,7 @@ class Main implements EventListenerObject {
             textarea.value += u.mostrar() + "\n";
         }
     }
-    private buscarDevices() {
+    private buscarDevices() { //Función que busca los devices del backend y los postea en consola, cuadro de texto y los ordena en listado
         var textarea2=document.getElementById("textarea_2") as HTMLTextAreaElement;
         let xmlRequest = new XMLHttpRequest();
         xmlRequest.onreadystatechange= () => {
@@ -19,20 +20,62 @@ class Main implements EventListenerObject {
                     //console.log(xmlRequest.responseText,xmlRequest.readyState);
                     let respuesta=xmlRequest.responseText;
                     let datos: Array<Device>=JSON.parse(respuesta);
+
+                    let ul = document.getElementById("listaDisp");
+
                     for (let d of datos){
+                        let itemList= `<li class="collection-item avatar">
+                        <img src= `;
+                            if (d.type==1) {
+                               itemList+= `"./static/images/Lampara.jpg" alt="" class="circle"`;
+                            } else if (d.type==2) {
+                                itemList+= `"./static/images/Ventilador.jpg" alt="" class="circle"`;
+                            } else if (d.type==3) {
+                                itemList+= `"./static/images/Televisor.jpg" alt="" class="circle"`;
+                            }
+                            itemList += `<span class="title">${d.name}</span>
+                            <p>
+                                ${d.description}
+                            </p>
+                            <a href="#!" class="secondary-content">
+                                <div class="switch">
+                                    <label>
+                                         Off
+                                        <input type="checkbox"`;
+                                        itemList+= `nuevoAtt="${d.id}" id="cb_${d.id}"`
+                                            if (d.state) {
+                                                itemList+= `checked`;
+                                            }
+                                            itemList+= `>
+                                        <span class="lever"></span>
+                                        On
+                                    </label>
+                                </div>
+                            </a>
+                        </li>`
+                        
+                        ul.innerHTML+= itemList;
+
                         console.log(d.name);
                         textarea2.value += "-" + d.name + "\n";
+                        //if (d.state)==true
+                        //    let checkDevice=true
+
+                    }
+                    for (let d of datos) {
+                        let checkbox=document.getElementById("cb_"+ d.id);
+                        checkbox.addEventListener("click",this);
                     }
                 }else{
                     console.log("no encontré nada");
                 }
             }
         }
+        //xmlRequest.open("GET",`http://localhost:8000/otraCosa/"${d.id}"/"${d.name}"`,true)
         xmlRequest.open("GET","http://localhost:8000/devices",true)
         xmlRequest.send();
     }
-
-    private cargarUsuario(): void{
+    private cargarUsuario(): void{ //Función que permite cargar usuario considerando los item obligatorios
         let iNombre= <HTMLInputElement> document.getElementById("iNombre");
         let iDNI= <HTMLInputElement> document.getElementById("iDNI");
         let iPassword= <HTMLInputElement> document.getElementById("iPassword");
@@ -52,7 +95,26 @@ class Main implements EventListenerObject {
             pInfo.className="textoError";
         }
     }
+    private ejecutarPost(id:number,state:boolean) {
+        let xmlRequest = new XMLHttpRequest ();
+        xmlRequest.onreadystatechange=() => {
+            if (xmlRequest.readyState == 4) {
+                if (xmlRequest.status == 200) {
+                    console.log("Llego respuesta",xmlRequest.responseText);
+                } else {
+                    alert("Salió mal la consulta");
+                }
+            }
+        }
+        xmlRequest.open("POST","http://localhost:8000/device",true)
+        xmlRequest.setRequestHeader("Content-Type","application/json");
+        let s = {
+            id: id,
+            state:state};
+            xmlRequest.send(JSON.stringify(s));
+    }
     
+
     handleEvent(object: Event): void {
         let elemento=<HTMLElement> object.target;
         if ("btnListar"==elemento.id){
@@ -62,16 +124,24 @@ class Main implements EventListenerObject {
         }else if ("btnGuardar"==elemento.id){
             this.cargarUsuario();
             this.buscarPersonas();
+        }else if(elemento.id.startsWith("cb_")){
+            let checkbox = <HTMLInputElement>elemento;
+            console.log(checkbox.getAttribute("nuevoAtt"),checkbox.checked,elemento.id.substring(3,elemento.id.length));
+            this.ejecutarPost(parseInt(checkbox.getAttribute("nuevoAtt")),checkbox.checked);
         }
-        
-
     }
 }
 window.addEventListener("load", () => {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, "");
 
     let main1: Main = new Main();
     let boton = document.getElementById("btnListar");
+
     boton.addEventListener("click", main1);
     let botonGuardar = document.getElementById ("btnGuardar");
     botonGuardar.addEventListener("click",main1);
+
+    let checkbox=document.getElementById("cb");
+    checkbox.addEventListener("click",main1);
 });
